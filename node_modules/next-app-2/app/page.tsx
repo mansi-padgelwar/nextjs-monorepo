c0af56f60d84b3e093e9932d442123a0cc7677b5
mansi-padgelwar <mansi.padgelwar@airindia.com>
2023-12-18 15:27:02 +0530

    mono repo concept

---
+++ node_modules/next-app-2/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Button from "@nextjs-monorepo/shared/Button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>NEXT.js App 2</h1>
+      <Button text="App 2 clicked">Click me!</Button>
+    </div>
+  );
+}
---
+++ packages/shared/Button.js
@@ -0,0 +1,25 @@
+"use client";
+import React, { useEffect } from "react";
+
+const Button = ({ children, text }) => {
+  const handleClick = () => {
+    console.log("log click value", text);
+  };
+
+  return (
+    <button
+      id="myButton"
+      style={{
+        padding: "8px",
+        margin: "4px",
+        border: "1px solid black",
+        borderRadius: 16,
+      }}
+      onClick={handleClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
